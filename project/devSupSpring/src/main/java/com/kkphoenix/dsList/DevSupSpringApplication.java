412fb33c7cad928998152f28d14aeacf3314f88b
Kauã <kauaalvesworkplace@gmail.com>
2024-11-16 14:48:47 -0300

    Proprieties, Game, ORM

---
+++ project/devSupSpring/src/main/java/com/kkphoenix/dsList/DevSupSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kkphoenix.dsList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSupSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSupSpringApplication.class, args);
+	}
+
+}
---
+++ project/devSupSpring/src/test/java/com/kkphoenix/dsList/DevSupSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkphoenix.dsList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSupSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
